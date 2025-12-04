1f2822294b7e96579f5275301391c3606041c649
EvgenyStishenok99 <stishenok999@mail.ru>
2025-12-04 17:45:30 +0300

    coplite project with style components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import logo from './images/Rectangle-1.png'
+
+
+const AppContainer = styled.div`
+  width: 300px;
+    height: 500px;
+`;
+
+const CardImage = styled.img`
+  margin: 10px 10px 20px 10px;
+`;
+
+const Title = styled.div`
+  font-weight: 700;
+  font-size: 16px;
+  color: #000;
+  margin-bottom: 20px;
+  margin-left: 20px;
+`;
+
+const MainText = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 167%;
+  color: #abb3ba;
+  margin: 20px ;
+`;
+
+const ButtonsContainer = styled.div`
+  
+`;
+
+const FirstButton = styled.button`
+  border-radius: 5px;
+  border: none;
+  width: 86px;
+  height: 30px;
+  background: #4e71fe;
+  font-weight: 700;
+  font-size: 10px;
+  line-height: 200%;
+  color: #fff;
+  margin: 0 12px 22px 20px;`
+
+const SecondButton = styled.button`
+  border-radius: 5px;
+  border-color: #4e71fe;
+  width: 86px;
+  height: 30px;
+  background: #fff;
+
+  font-weight: 700;
+  font-size: 10px;
+  line-height: 200%;
+  color: #4e71fe;`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+
+        <CardImage
+          src={logo}
+          alt="картинка"
+        />
+        <Title>Headline</Title>
+        <MainText>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</MainText>
+        <ButtonsContainer>
+          <FirstButton>See more</FirstButton>
+          <SecondButton>Save</SecondButton>
+        </ButtonsContainer>
+
+    </AppContainer>
   );
 }
 
